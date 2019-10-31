3113f67b641b739bc6a3782572f001a17b1face4
rodolforivr <rodolforiverado@gmail.com>
2019-10-31 08:25:21 -0400

    Web casi terminada falta terminar la sección 'contact-me.html'

---
+++ js/app.js
@@ -0,0 +1,12 @@
+$(document).ready(main);
+
+var counter = 1;
+
+
+
+function main () {
+    $('.button__responsive--icon').click( function() {
+        $('.header__head--menu').toggle();
+        
+    });
+};
